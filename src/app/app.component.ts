1146fb8bf7e458abb41facc7d4673cf2b96846c7
Stanislava Pavlova <stanislavap2002@gmail.com>
2024-05-02 18:40:44 +0300

    Add functionality for saving and deleting notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,51 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+type Note = {
+  id: string;
+  title: string;
+  content: string;
+  isActive: boolean;
+};
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'note-keeper';
+  title = 'NoteKeeper';
+
+  public notes: Note[] = [];
+  public noteTitle: string = '';
+  public noteContent: string = '';
+
+  public saveNote() {
+    this.notes.push({
+      id: crypto.randomUUID(),
+      title: this.noteTitle,
+      content: this.noteContent,
+      isActive: false,
+    });
+
+    this.resetTempData();
+  }
+
+  private resetTempData() {
+    this.noteTitle = '';
+    this.noteContent = '';
+  }
+
+  public makeActive(note: Note) {
+    this.notes.forEach((n) => (n.isActive = false));
+    note.isActive = true;
+  }
+
+  public deleteNote(note: Note) {
+    this.notes = this.notes.filter((n) => n.id !== note.id);
+  }
 }
